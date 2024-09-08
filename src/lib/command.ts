1d56aafcd5ac5d23f8ea560d6af6ce8d0be140f3
kalmonJ <kalmonkk69@gmail.com>
2024-09-07 22:57:01 -0300

    refactor: done

--- src/lib/command.ts
+++ src/lib/command.ts
@@ -1,90 +1,101 @@
-// import { isAsyncFunction } from "util/types";
-// import { CommandFactory } from "./command-factory";
+import { isAsyncFunction } from "util/types";
 
 export type CommandType = "option" | "argument";
 
 type Maybe<T> = T | undefined;
-
 
 type OptionBaseConfig = {
   type: "option";
   value: string;
   isRoot: false;
   ref: string;
-  arguments: Argument[] | Argument
-}
+  arguments: Argument[] | Argument;
+};
 
 type OptionRootConfig = {
-  type: "option"
-  value: string
-  isRoot: true
-  arguments: Argument[] | Argument
-  ref?: string
-}
+  type: "option";
+  value: string;
+  isRoot: true;
+  arguments: Argument[] | Argument;
+  ref?: string;
+};
 
-type OptionUnion = OptionBaseConfig | OptionRootConfig
+type OptionUnion = OptionBaseConfig | OptionRootConfig;
 
-type OptionConfig = OptionUnion
-
+type OptionConfig = OptionUnion;
 
 type Argument = {
-  type: "string" | "number"
-  name: string
-  action: Maybe<Function>
-}
+  type: "string" | "number";
+  name: string;
+  action: Maybe<Function>;
+};
 
 type RegisterOptionProps = OptionConfig & {
-  node: OptionNode
-}
+  node: OptionNode;
+};
 
 export class OptionNode {
-  type: "option" = "option"
+  type: "option" = "option";
   value: string;
   ref: Maybe<string>;
   next: Maybe<OptionNode>;
   isRoot: boolean;
   arguments: Argument[] | Argument;
 
-  constructor(value: string, isRoot: boolean, optionArguments: Argument[] | Argument, ref: Maybe<string>) {
+  constructor(
+    value: string,
+    isRoot: boolean,
+    optionArguments: Argument[] | Argument,
+    ref: Maybe<string>,
+  ) {
     this.value = value;
-    this.isRoot = isRoot
-    this.arguments = optionArguments
-    this.ref = ref
+    this.isRoot = isRoot;
+    this.arguments = optionArguments;
+    this.ref = ref;
   }
 }
 
 export class Command {
   root: Map<string, OptionNode> = new Map();
   private currentRoot?: OptionNode;
-  private commands: string[] = []
 
   option(config: OptionConfig) {
-    const node = new OptionNode(config.value, config.isRoot, config.arguments, config.ref)
+    const node = new OptionNode(
+      config.value,
+      config.isRoot,
+      config.arguments,
+      config.ref,
+    );
 
     if (node.isRoot && !this.root.size) {
-      this.root.set(this.commandToCode(node.value), node)
-      return this
+      this.root.set(this.commandToCode(node.value), node);
+      return this;
     }
 
     if (!node.isRoot && node.ref) {
-      const code = this.commandToCode(node.ref)
-      const rootNode = this.root.get(code)
-      if (!rootNode) throw new Error("Invalid reference")
-      this.registerOption({ ...config, node: rootNode })
+      const code = this.commandToCode(node.ref);
+      const rootNode = this.root.get(code);
+      if (!rootNode) throw new Error("Invalid reference");
+      this.registerOption({ ...config, node: rootNode });
     }
 
     return this;
   }
 
   private registerOption(props: RegisterOptionProps) {
-    const node = new OptionNode(props.value, props.isRoot, props.arguments, props.ref)
+    const node = new OptionNode(
+      props.value,
+      props.isRoot,
+      props.arguments,
+      props.ref,
+    );
 
     if (!props.node.next) {
-      props.node.next = node
-      return
+      props.node.next = node;
+      return;
     }
 
-    this.registerOption({ ...props, node: props.node.next })
+    this.registerOption({ ...props, node: props.node.next });
   }
 
   validate(...commands: string[]) {
@@ -103,84 +114,80 @@ export class Command {
       if (!isValidCommand) throw new Error(`invalid command ${command}`);
     }
 
-    this.commands = commands
     this.currentRoot = rootNode;
 
     return this;
   }
 
-  // exec() {
-  //   if (!this.currentRoot) throw new Error("No currentRoot found");
-  //   this.recursiveExecution(this.currentRoot);
-  // }
+  exec() {
+    if (!this.currentRoot) throw new Error("No currentRoot found");
+    this.recursiveExecution(this.currentRoot);
+  }
 
-  // private async recursiveExecution(node: CommandNode) {
+  private async recursiveExecution(node: OptionNode) {
 
-  //   if (node.action && isAsyncFunction(node.action)) {
-  //     await node.action()
-  //   } else {
-  //     if (node.action) {
-  //       node.action()
-  //     }
-  //   }
+    if (!node.next) {
+      return
+    }
 
-  //   if (this.commands.at(-1) === node.value) {
-  //     return
-  //   }
+    if (Array.isArray(node.arguments)) {
+      node.arguments.forEach(async argument => {
+        if (isAsyncFunction(argument.action) && argument.action) {
+          await argument.action()
+        } else {
+          if (argument.action) {
+            argument.action()
+          }
+        }
+      })
+    } else {
+      if (isAsyncFunction(node.arguments) && node.arguments.action) {
+        await node.arguments.action()
+      } else {
+        if (node.arguments.action) {
+          node.arguments.action()
+        }
+      }
+    }
 
-  //   node.childrens.forEach(childNode => {
-  //     this.recursiveExecution(childNode)
-  //   })
-  // }
+    this.recursiveExecution(node.next)
+  }
 
-  // inspect(command: string) {
-  //   let value: CommandNode | undefined;
+  inspect(command: string) {
+    let value: OptionNode | undefined;
 
-  //   this.root.forEach((node) => {
-  //     value = this.findNode(node, command);
+    this.root.forEach((node) => {
+      value = this.findNode(node, command);
+    });
+    return value;
+  }
 
-  //   });
-  //   return value;
-  // }
+  private findNode(node: OptionNode, command: string) {
+    if (!node.next) {
+      return;
+    }
 
-  // private findNode(
-  //   node: CommandNode,
-  //   command: string,
-  //   findNode: CommandNode | undefined = undefined,
-  // ) {
-  //   if (!node.childrens.size) {
-  //     findNode = undefined;
-  //     return findNode;
-  //   }
+    if (node.value === command) {
+      return node;
+    }
 
-  //   if (node.value === command) {
-  //     findNode = node;
-  //     return findNode;
-  //   }
+    this.findNode(node.next, command);
 
-  //   node.childrens.forEach((childNode) => {
-  //     findNode = this.findNode(childNode, command, findNode);
-  //   });
+    return node;
+  }
 
-  //   return findNode;
-  // }
-
-  private recursiveValidation(
-    node: OptionNode,
-    command: string,
-    equal: boolean,
-  ) {
+  private recursiveValidation(node: OptionNode, command: string, equal: boolean) {
     if (node.value === command) {
       equal = true;
       return equal;
     }
 
     if (!node.next) {
-      equal = false
-      return equal
+      equal = false;
+      return equal;
     }
 
-    this.recursiveValidation(node.next, command, equal)
+    this.recursiveValidation(node.next, command, equal);
 
     return equal;
   }

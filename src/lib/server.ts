4e9db359f43ab9b3819791553311ef917bafb633
kalmonJ <kalmonkk69@gmail.com>
2024-10-05 13:51:28 -0300

    feat: add proxy server

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,20 +1,34 @@
 import express from "express"
+import { createProxyMiddleware, responseInterceptor } from "http-proxy-middleware"
 
 const app = express()
 app.use(express.json())
 
 export const startServer = (url: string, port: number) => {
 
-  app.listen(port, () => {
-    console.log(`server is listen on port:${port}`)
+  const proxyMiddleware = createProxyMiddleware({
+    target: url,
+    changeOrigin: true,
+    selfHandleResponse: true,
+    on: {
+      proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        const response = responseBuffer.toString("utf-8")
+        console.log(response, "resposta")
+        return response
+      })
+    }
   })
+
+  app.listen(port, () => {
+    console.log(`Proxy server is listen on http://localhost:${port}`)
+  })
+
+  app.use(proxyMiddleware)
+
 
   process.on("uncaughtException", (error: any) => {
     if (error.code === "EACCES") {
       console.log(`Port ${error.port} is already in use, try again with a different port argument.`)
     }
   })
-
-
-
 }
